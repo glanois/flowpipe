a2e87a95e42ec43ffa593ff6727a9d22d03aea42
Gerard Lanois <gerard@lanois.com>
2019-11-15 16:35:50 -0800

    Add comment explaining what it does.

--- examples/flowserver.py
+++ examples/flowserver.py
@@ -14,6 +14,7 @@ class Application(flowpipe.INode):
         flowpipe.OutputPlug('output', self)
 
     def compute(self, **args):
+        # Echo the input back as the output.
         return { 'output' : args['input'] }
 
 
